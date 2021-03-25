630341fcd6a4e09c880899003f081b64bdd60ba7
gsem73 <gh5t@yandex.ru>
2021-03-25 22:47:41 +0300

    Б9: Добавлены обязательные блоки кода

--- source/js/utils.js
+++ source/js/utils.js
@@ -24,7 +24,9 @@ const debounce = function(callback, delay) {
   let isCooldown = false;
 
   return function() {
-    if (isCooldown) return;
+    if (isCooldown) {
+      return;
+    }
     callback.apply(this, arguments);
     isCooldown = true;
     setTimeout(() => isCooldown = false, delay);
